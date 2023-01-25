6a88f54aba91d9dc61fa8ec3e281b1c8618d6ee7
Selvarathinam .M <119609131+Selvarathinam27@users.noreply.github.com>
2023-01-25 10:42:43 +0530

    Add files via upload

---
+++ loaves.py
@@ -0,0 +1,11 @@
+loaves = int(input("Enter number of loaves of day old bread: "))
+
+regular_price = loaves * 185
+
+discount = regular_price * 0.6
+
+total_price = regular_price - discount
+
+print("Regular price: {:.2f} rupees".format(regular_price))
+print("Discount: {:.2f} rupees".format(discount))
+print("Total price: {:.2f} rupees".format(total_price))
